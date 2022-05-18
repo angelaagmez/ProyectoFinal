ab3e189fe76f129657c500fd250ed84ff4543567
Ángela Bonilla <44753691+angelaagmez@users.noreply.github.com>
2022-05-18 17:50:13 +0200

    Delete plataformaexamenes directory

--- plataformaexamenes/src/main/java/com/iesalixar/proyecto/services/UsuarioService.java
+++
@@ -1,9 +0,0 @@
-package com.iesalixar.proyecto.services;
-
-import com.iesalixar.proyecto.model.Usuario;
-
-public interface UsuarioService {
-	public Usuario insertUsuario(Usuario usuario);
-
-	public Object findUserByUsername(String userName);
-}
